742901907825f398d270ea0478f5b0673d8cbde6
Wellerson Roberto <wellerson.roberto@geopostenergy.com>
2022-11-12 19:31:42 -0300

    Iniciando

---
+++ Entitites/BrazilBlock.cs
@@ -0,0 +1,23 @@
+﻿using PanelaTI.AbstractFactory.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanelaTI.AbstractFactory.Entitites
+{
+    public class BrazilBlock : IField
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Name { get; set; }
+
+        public BrazilBlock()
+        {
+            StartDate = DateTime.Now;
+            EndDate = StartDate.AddYears(5);
+
+        }
+    }
+}
